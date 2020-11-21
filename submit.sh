44a9cae606010637bab7b070be1b7e67117722d2
BingqingCheng <tonicbq@gmail.com>
2020-11-21 13:24:01 +0000

    update

--- submit.sh
+++ submit.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#SBATCH --account T2-CS061-CPU   
-## CHENG-SL3-CPU
+#SBATCH --account CHENG-SL3-CPU
 #SBATCH --partition skylake-himem
 #SBATCH --nodes 1
 #SBATCH --ntasks 32
-#SBATCH --time 36:00:00
+#SBATCH --time 12:00:00
 #SBATCH -J benchml
 
 #module purge
@@ -23,8 +22,10 @@ n=0
 
 for m in $(grep '\- ' /rds/project/t2_vol5/rds-t2-cs061/linear-regression-benchmarks/model_list_on_csd3.txt | awk '{print $2}' | grep 'bmol' | shuf); do
 
-if [ ! -e ${m}.log ] && [ $n -lt 8 ] ; then
-       ((n++))
+if [ ! -e is_csh-${m}.json ] && [ -e ${m}.log ] && [ $n -lt 2 ] ; then
+#if [ ! -e ${m}.log ] && [ $n -lt 1 ] ; then
+
+((n++))
 	bml -m benchmark --meta meta.json --models "^${m}$" --benchmark_json ${prefix}-${m}.json --use_ase &> ${m}.log &
 fi
 done
